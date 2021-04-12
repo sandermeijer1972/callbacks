bf9ea23d23fe7005660d5c91021dca2402b70021
Sander Meijer <sandertje1972@hotmail.com>
2021-04-12 10:51:11 +0200

    changed script

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-huiswerkMaken = (vak, callback) => {
-    console.log("Ok, ok, ik ga nu mijn ${vak} huiswerk maken")
-}
+const huiswerkMaken = (vak, callback) => {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+    setTimeout(function() {
+        callback();
+    }, 5000);
+};
+
+const klaarMetHuiswerk = () => {
+    console.log(`Kijk Paps/Mams, ik ben klaar met mijn huiswerk!`);
+};
+
+huiswerkMaken("biologie", klaarMetHuiswerk);
